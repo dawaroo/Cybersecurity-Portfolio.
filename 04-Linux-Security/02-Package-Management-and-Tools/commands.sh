82ee29cda49fff94faccefba19d5c3d5959f30f3
Daniel waser C security <waserdaniel2000@gmail.com>
2026-01-12 11:09:50 -0600

    Update commands.sh

--- 04-Linux-Security/02-Package-Management-and-Tools/commands.sh
+++ 04-Linux-Security/02-Package-Management-and-Tools/commands.sh
@@ -1,2 +1,19 @@
 # Commands used to install and manage security tools
 
+# Verify that APT is installed
+apt --version
+
+# Update package lists
+sudo apt update
+
+# Install Suricata (IDS/IPS tool)
+sudo apt install suricata
+
+# Remove Suricata
+sudo apt remove suricata
+
+# Install tcpdump for packet capture and traffic analysis
+sudo apt install tcpdump
+
+# Reinstall Suricata
+sudo apt install --reinstall suricata
